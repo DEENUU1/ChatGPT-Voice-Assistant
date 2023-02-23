f3593ac6cee0829fc6c0b476c5e150c0aa7cf30a
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-02-23 15:16:48 +0100

    Text to speech function

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import asyncio
 from docx import Document
 from summaryzer import return_article_summary
+import pyttsx3
+
+
+def return_speech(summary):
+    engine = pyttsx3.init()
+    engine.say(summary)
+    engine.runAndWait()
 
 
 async def main_async():
     document = Document()
+
     async for summary, article_url in return_article_summary():
         print(summary, article_url)
+
+        return_speech(summary)
+
         document.add_paragraph(
             summary + ' ' + article_url
         )
