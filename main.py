55fef5634425869e2d2dadaab04ee80ba1b94ada
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-02-23 16:03:35 +0100

    Config file and ability to change the language and country_code to choose from which country return news

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import asyncio
 from docx import Document
 from summaryzer import return_article_summary
 import pyttsx3
+import configparser
 
 
-def return_speech(summary):
+def return_speech(summary: str):
     engine = pyttsx3.init()
     engine.say(summary)
     engine.runAndWait()
 
 
 async def main_async():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    language = config.get('NEWS SUMMARIZER', 'language')
+    country_code = config.get('NEWS SUMMARIZER', 'country_code')
+
     document = Document()
 
-    async for summary, article_url in return_article_summary():
+    async for summary, article_url in return_article_summary(country_code, language):
         print(summary, article_url)
 
         return_speech(summary)
-
         document.add_paragraph(
             summary + ' ' + article_url
         )
--- news.py
+++ news.py
@@ -14,8 +14,8 @@ class News:
     def __init__(self):
         self.api_key = os.getenv('NEWS_KEY')
 
-    def _get_news_urls(self):
-        base_url = f'https://newsapi.org/v2/top-headlines?country=PL&apiKey={self.api_key}'
+    def _get_news_urls(self, country_code: str):
+        base_url = f'https://newsapi.org/v2/top-headlines?country={country_code}&apiKey={self.api_key}'
         result = get(base_url)
         json_result = json.loads(result.content)
         articles = json_result['articles']
