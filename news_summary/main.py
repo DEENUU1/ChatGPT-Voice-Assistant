ec4a93c48d700757b70f9a838f215100344d54d2
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-02-27 18:00:12 +0100

    Welcome message, returning current week day and time

--- news_summary/main.py
+++ news_summary/main.py
@@ -3,6 +3,8 @@ from docx import Document
 from summaryzer import return_article_summary
 import pyttsx3
 import configparser
+from datetime import datetime
+import calendar
 
 
 def return_speech(summary: str):
@@ -11,13 +13,29 @@ def return_speech(summary: str):
     engine.runAndWait()
 
 
+def welcome_message(user_name: str) -> str:
+    get_time = datetime.now()
+    get_date = datetime.today()
+    current_date = calendar.day_name[get_date.weekday()]
+    current_time = get_time.strftime("%H:%M")
+    message = f"Welcome {user_name} today is {current_date} {current_time}" \
+              f" You are about to hear today's news release. Have a good day and see" \
+              f" you later!"
+    return message
+
+
 async def main_async():
     config = configparser.ConfigParser()
     config.read('config.ini')
     language = config.get('NEWS SUMMARIZER', 'language')
     country_code = config.get('NEWS SUMMARIZER', 'country_code')
+    user_name = config.get('NEWS SUMMARIZER', 'user_name')
 
     document = Document()
+
+    welcome_user = welcome_message(user_name)
+    print(welcome_user)
+    return_speech(welcome_user)
 
     async for summary, article_url in return_article_summary(country_code, language):
         print(summary, article_url)
@@ -28,6 +46,7 @@ async def main_async():
         )
 
     document.save('news.docx')
+
 
 if __name__ == '__main__':
     asyncio.run(main_async())
