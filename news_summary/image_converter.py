4603ab6ba3b9a8deb3732e2fac1d98ac2e20051d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-06 23:26:05 +0100

    V0.3 Image to text, taking notes, voice recognizer, voice chat with AI and more

--- news_summary/image_converter.py
+++ news_summary/image_converter.py
@@ -1,22 +1,3 @@
-# import cv2
-# from pytesseract import pytesseract
-# import os
-# from PIL import Image
-#
-#
-# SOURCE_PATH = 'images/Screenshot_1.png'
-# TESSERACT_PATH = 'tesseract/tesseract.exe'
-#
-#
-# def image_to_text():
-#     image = Image.open(SOURCE_PATH)
-#     pytesseract.tesseract_cmd = TESSERACT_PATH
-#     text = pytesseract.image_to_string(image, lang='pol')
-#     return text
-#
-# print(image_to_text())
-
-
 import requests
 import json
 import configparser
@@ -29,6 +10,7 @@ USERNAME = "KACPER"
 
 
 def image_to_text(image_name: str) -> str:
+    """ This function allows to convert image into text with OCR API """
     with open(f'images/{image_name}', 'rb') as image_file:
         image_data = image_file.read()
 
